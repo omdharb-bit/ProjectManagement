7502876e7231bf4a6acf1dfb4f7f7931acc2ebb9
Om Dhar Badgaiyan <omdharb@gmail.com>
2025-11-08 18:21:18 +0530

    env

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+import dotenv from "dotenv";
+
+dotenv.config({
+path:"./.env",
+})
+
+let myusername = process.env.username;
+
+console.log("value:", myusername);
+
 console.log("Start of the backend project");
