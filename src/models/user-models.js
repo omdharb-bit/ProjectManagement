f009511f237fc8ba16640f7c759fec5e0b8914e5
Om Dhar Badgaiyan <omdharb@gmail.com>
2025-11-13 15:32:52 +0530

    o

--- src/models/user-models.js
+++ src/models/user-models.js
@@ -1,66 +1,71 @@
-import mongoose,{Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 import bcrypt from "bcrypt";
 
-const userSchema = new Schema({
-  avatar: {
-    type: {
-      url: String,
-      localPath: String,
-    },
-    default: {
-      url: `https://placehold.co/200x200`,
-      localPath: "",
-    },
-  },
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
-    index: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim:true
-  },
-  password: {
-    type: String,
-    required:[true,"Password is required"]
-  },
-  isEmailVerified: {
-    type: Boolean,
-    default: fasle
-  },
-  refreshToken: {
-  type: String
-  },
-  forgotPasswordToken: {
-  type: String
-  },
-  forgotPasswordExpiry: {
-  type: String  
-  },
-  emailVerificationToken: {
-  type:String
-  },
-  emailVerificationExpiry: {
-  type: Date
-  },
-},
+const userSchema = new Schema(
   {
-  timestamps: true,
+    avatar: {
+      type: {
+        url: String,
+        localPath: String,
+      },
+      default: {
+        url: `https://placehold.co/200x200`,
+        localPath: "",
+      },
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    isEmailVerified: {
+      type: Boolean,
+      default: fasle,
+    },
+    refreshToken: {
+      type: String,
+    },
+    forgotPasswordToken: {
+      type: String,
+    },
+    forgotPasswordExpiry: {
+      type: String,
+    },
+    emailVerificationToken: {
+      type: String,
+    },
+    emailVerificationExpiry: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
   },
 );
 
 userSchema.pre("save", async function (next) {
-  if (!this.isModified("Password")) return next()
-  
- this.password= await bcrypt.hash(this.password, 10)
-  next()
-})
+  if (!this.isModified("Password")) return next();
 
-export const User= mongoose.model("User",userSchema)
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+export const User = mongoose.model("User", userSchema);
