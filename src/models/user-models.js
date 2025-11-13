cf891cff490accbc9bc5d6db1d935e1f7be785e5
Om Dhar Badgaiyan <omdharb@gmail.com>
2025-11-13 15:17:18 +0530

    g

--- src/models/user-models.js
+++ src/models/user-models.js
@@ -1,5 +1,5 @@
 import mongoose,{Schema} from "mongoose";
-
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema({
   avatar: {
@@ -55,5 +55,12 @@ const userSchema = new Schema({
   timestamps: true,
   },
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("Password")) return next()
+  
+ this.password= await bcrypt.hash(this.password, 10)
+  next()
+})
 
 export const User= mongoose.model("User",userSchema)
